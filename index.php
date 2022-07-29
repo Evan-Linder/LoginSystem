3f76e6c67cdc9f58fbfc971f8078a817e9c7055b
Evan Linder <mo9564ej@go.minnstate.edu>
2022-07-28 22:31:22 -0500

    3rd commit Reg Form completed

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
 <body>
      <form action="login.php" method="post">
      	<h2>LOGIN</h2>
-		<!-- initializes the  error messages if user does not enter information. -->
-
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
@@ -19,6 +17,7 @@
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
+          <a href="signup.php" class="ca">Create an account</a>
      </form>
 </body>
 </html>
